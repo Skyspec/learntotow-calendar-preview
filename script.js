22653cedf73dec5e91a8597b2360b5d08348c6bf
Skyspec <admin@skyspec.com.au>
2025-07-01 04:37:14 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const calendarEl = document.getElementById('calendar');
+
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    height: 'auto',
+    events: [] // This will be populated with real data later
+  });
+
+  calendar.render();
+});
